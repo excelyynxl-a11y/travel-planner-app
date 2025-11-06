6f53dc746b873b1c4466bc71fa97b679d09d0b80
excel <elim0079@student.monash.edu>
2025-11-06 16:23:44 +0800

    setup prisma

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, env } from "prisma/config";
+import "dotenv/config"; // from env instruction
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
