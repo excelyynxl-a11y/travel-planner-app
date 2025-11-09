5696dd19302d7109c36cd9c3276b2ba4d219729d
excel <elim0079@student.monash.edu>
2025-11-09 11:44:52 +0800

    add images config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      hostname:  "aihepieaxg.ufs.sh",
+    }]
+  }
 };
 
 export default nextConfig;
